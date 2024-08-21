3ec3b3526abdbb9696971b547a7f831460729c9a
ebube <aaronebube123@gmail.com>
2024-08-21 17:32:02 +0100

    feat: application scaffold

---
+++ components/SearchInput.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { View, TextInput, StyleSheet } from "react-native";
+const SearchInput = () => {
+  const [search, setSearch] = useState("");
+  return (
+    <View style={styles.searchContainer}>
+      <TextInput
+        placeholder="search something..."
+        value={search}
+        onChangeText={(text) => setSearch(text)}
+      />
+    </View>
+  );
+};
+
+export default SearchInput;
+
+
+const styles = StyleSheet.create({
+    searchContainer: {
+        flexDirection: 'row',
+        gap: 8,
+        borderWidth: 1,
+        borderRadius: 8,
+        padding: 12
+    },
+})
